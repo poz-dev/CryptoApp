060954adb8ebbc575027c6aa4d035d40d5f79f44
poz-dev <kresimir.ivanjko@icloud.com>
2024-01-31 11:57:22 +0100

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Kresimir Ivanjko on 31.01.2024..
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
